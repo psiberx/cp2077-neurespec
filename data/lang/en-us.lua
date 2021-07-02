fabd18d64427cdaff21fcf6020b24563d4f72b67
pv <p@siberx.dev>
2021-07-02 19:17:24 +0300

    Update english translation

--- data/lang/en-us.lua
+++ data/lang/en-us.lua
@@ -1,4 +1,6 @@
 return {
+	['UI-Menus-Perks-ResetPerks'] = 'Reset Perks',
+	['UI-Menus-Perks-ResetConfirmation'] = 'This action will reset all acquired perks and allow you to redestribute perk points in the progression tree. Do you want to continue?',
 	['UI-Menus-Attributes-ResetAttributes'] = 'Reset Attributes',
 	['UI-Menus-Attributes-ResetConfirmation'] = 'This action will reset all acquired attributes and allow you to redestribute attribute points in the progression tree. Do you want to continue?',
 }
