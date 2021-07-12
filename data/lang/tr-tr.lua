c8e2a78cfcf0b843cf3da0967540dbac842c9232
pv <p@siberx.dev>
2021-07-12 07:01:40 +0300

    Add turkish translation

---
+++ data/lang/tr-tr.lua
@@ -0,0 +1,8 @@
+-- Translation by theosaa
+return {
+	['Hold to Return'] = 'Geri dönmek için basılı tutun',
+	['UI-Menus-Perks-ResetPerks'] = 'Kazanımları sıfırla',
+	['UI-Menus-Perks-ResetConfirmation'] = 'Bu eylem elde edilen tüm kazanımlarınızı sıfırlayacak ve ilerleme ağacında kazanım puanlarınızı yeniden dağıtmanıza izin vericektir. Devam etmek istiyor musun?',
+	['UI-Menus-Attributes-ResetAttributes'] = 'Nitelikleri sıfırla',
+	['UI-Menus-Attributes-ResetConfirmation'] = 'Bu eylem elde edilen tüm niteliklerinizi sıfırlayacak ve ilerleme ağacında nitelik puanlarını yeniden dağıtmanıza izin verecektir. Devam etmek istiyor musun?',
+}
