643affcc0892cc42892723fc170f7cedd85979ae
pv <p@siberx.dev>
2021-09-01 22:02:33 +0300

    Add french translation

---
+++ data/lang/fr-fr.lua
@@ -0,0 +1,8 @@
+-- Translation by AllStarze
+return {
+	['Hold to Return'] = 'Maintenir pour revenir',
+	['UI-Menus-Perks-ResetPerks'] = 'Réinitialiser les avantages',
+	['UI-Menus-Perks-ResetConfirmation'] = 'Cette action réinitialisera tous les avantages acquis et vous permettra de redistribuer les points d\'avantages dans l\'arborescence de progression. Voulez-vous continuer ?',
+	['UI-Menus-Attributes-ResetAttributes'] = 'Réinitialiser les attributs',
+	['UI-Menus-Attributes-ResetConfirmation'] = 'Cette action réinitialise tous les attributs acquis et vous permet de réaffecter des points d\'attribut dans l\'arborescence de progression. Voulez-vous continuer ?',
+}
