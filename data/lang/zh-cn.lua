14c2cebd0d68c914166ad255f2b5167ebe8fe176
pv <p@siberx.dev>
2021-09-04 20:46:37 +0300

    Add chinese translation

---
+++ data/lang/zh-cn.lua
@@ -0,0 +1,8 @@
+-- Translation by mengxinlian0712
+return {
+	['Hold to Return'] = '按住以便返回',
+	['UI-Menus-Perks-ResetPerks'] = '重置专长',
+	['UI-Menus-Perks-ResetConfirmation'] = '该操作会重置所有已经获得的专长，允许你在成长树里重新分配专长点数。是否继续？',
+	['UI-Menus-Attributes-ResetAttributes'] = '重置属性点',
+	['UI-Menus-Attributes-ResetConfirmation'] = '该操作会重置所有已分配的属性点，允许您重新分配属性点。是否继续？',
+}
