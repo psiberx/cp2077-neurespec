2bd7878530b3c89506cbc4d3c0aa614a977b81a6
pv <p@siberx.dev>
2022-12-02 13:25:22 +0300

    Add translations

---
+++ data/lang/es-es.lua
@@ -0,0 +1,8 @@
+-- Translation by MrThehail
+return {
+	['Hold to Return'] = 'Mantén para devolver',
+	['UI-Menus-Perks-ResetPerks'] = 'Reiniciar habilidades',
+	['UI-Menus-Perks-ResetConfirmation'] = 'Esta acción reiniciará todos los puntos de habilidades adquiridos y te permitirá redistribuir tus puntos de habilidades en el árbol de progreso. ¿Quieres continuar?',
+	['UI-Menus-Attributes-ResetAttributes'] = 'Reiniciar atributos',
+	['UI-Menus-Attributes-ResetConfirmation'] = 'Esta acción reiniciará todos los puntos de atributos adquiridos y te permitirá redistribuir tus puntos de atributo en el árbol de progreso. ¿Quieres continuar?',
+}
---
+++ data/lang/ko-kr.lua
@@ -0,0 +1,8 @@
+-- Translation by D2AF
+return {
+    ['Hold to Return'] = '돌려받으려면 누르고 있기',
+    ['UI-Menus-Perks-ResetPerks'] = '특전 초기화',
+    ['UI-Menus-Perks-ResetConfirmation'] = '이 작업을 수행하면 획득한 모든 특전을 초기화하고 스킬 트리에서 특전 포인트를 재분배할 수 있습니다. 계속하시겠습니까?',
+    ['UI-Menus-Attributes-ResetAttributes'] = '특성 초기화',
+    ['UI-Menus-Attributes-ResetConfirmation'] = '이 작업을 수행하면 획득한 모든 특성을 초기화하고 특성 트리에서 특성 포인트를 재분배할 수 있습니다. 계속하시겠습니까?',
+}
